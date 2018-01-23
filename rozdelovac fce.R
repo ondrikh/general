34675966dbd1b85cbec4d8219df3675a3d4ea3b8
ondrikh <ondrah23@gmail.com>
2018-01-23 15:08:41 +0100

    Update rozdelovac fce.R

--- rozdelovac fce.R
+++ rozdelovac fce.R
@@ -1,6 +1,6 @@
 rozdelovac = function (data, seed=1989, ratio = 0.8, type = "train"){
   set.seed(seed)
-  if (type == "train") {temp = data_Mort
+  if (type == "train") {temp = data
   temp$split = 0
   temp$split = unlist(lapply(temp$split, function(X) runif(1,0,1)))
   output = temp[temp$split<=0.8,1:ncol(data)]
