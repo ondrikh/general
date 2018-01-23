fc1bc7ee6fe918a48a15f570810ca045232f5052
ondrikh <ondrah23@gmail.com>
2018-01-23 15:07:14 +0100

    Add files via upload

---
+++ rozdelovac fce.R
@@ -0,0 +1,14 @@
+rozdelovac = function (data, seed=1989, ratio = 0.8, type = "train"){
+  set.seed(seed)
+  if (type == "train") {temp = data_Mort
+  temp$split = 0
+  temp$split = unlist(lapply(temp$split, function(X) runif(1,0,1)))
+  output = temp[temp$split<=0.8,1:ncol(data)]
+  }
+  else if (type == "test") {temp = data
+  temp$split = 0
+  temp$split = unlist(lapply(temp$split, function(X) runif(1,0,1)))
+  output = temp[temp$split>0.8,1:ncol(data)]}
+  
+  return (output)
+}
